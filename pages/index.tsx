fc088677d318aebd6fba35908d3b3db3c08b7816
Troy Chaplin <troy.chaplin@gmail.com>
2023-01-22 15:14:33 -0500

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
+import { Button } from "@/components/Button";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
